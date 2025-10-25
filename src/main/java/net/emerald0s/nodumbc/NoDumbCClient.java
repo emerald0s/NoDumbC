4167a9bf8db4287a0dc60039f31216e2f7f1b663
emerald0s <eyal.shaham@gmail.com>
2025-10-25 18:10:10 +0300

    Update Server Only

--- src/main/java/net/emerald0s/nodumbc/NoDumbCClient.java
+++
@@ -1,9 +0,0 @@
-package net.emerald0s.nodumbc;
-
-import net.fabricmc.api.ClientModInitializer;
-public class NoDumbCClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-
-    }
-}
